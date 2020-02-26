7741e684d574e3dc2beb397d327eb02a48f228d3
ddawson235 <daviddawson90@gmail.com>
2020-02-26 22:23:39 +1100

    General clean and updated creds file for local test

---
+++ cypress/support/commands.js
@@ -0,0 +1,34 @@
+"use strict";
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", function() {
+  return cy.request(
+    "https://test.salesforce.com/?un=" +
+      Cypress.env("username") +
+      "&pw=" +
+      Cypress.env("password")
+  );
+});
