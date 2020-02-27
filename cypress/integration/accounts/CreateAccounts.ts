eda5fe7f09d08e80c9f089617b41c9217ef93034
Pavan Kumar Ananta <>
2020-02-27 11:08:36 +1100

    Move to before so its common for all the tests

--- cypress/integration/accounts/CreateAccounts.ts
+++ cypress/integration/accounts/CreateAccounts.ts
@@ -1,15 +1,22 @@
 import * as faker from "faker";
 describe("New Account Creation", function() {
-  it("Create Account", function() {
+  let instanceUrl:any;
+  before(function() {
+    // runs once before all tests in the block
     cy.exec(
       'sfdx force:org:display --json | sed -E "s/[[:cntrl:]][[0-9]{1,3}m//g"'
     ).then(response => {
       let result = JSON.parse(response.stdout).result;
       let sessionId = result.accessToken;
-      let instanceUrl = result.instanceUrl;
+      instanceUrl = result.instanceUrl;
 
       cy.request(`${instanceUrl}/secur/frontdoor.jsp?sid=${sessionId}`);
-      cy.visit(`${instanceUrl}/lightning/o/Account/list`);
+      
+  })
+})
+  it("Create Account", function() {
+    var companyName = faker.company.companyName();
+    cy.visit(`${instanceUrl}/lightning/o/Account/list`);
       cy.get(".forceActionLink[title=New]")
         .should("be.visible")
         .click();
@@ -18,8 +25,7 @@ describe("New Account Creation", function() {
         .contains("Account Name")
         .get("input[type=text]")
         .eq(1)
-        .type(faker.company.companyName());
+        .type(companyName);
       cy.get(".slds-button[title=Save]").click();
     });
   });
-});
